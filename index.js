0b89980b9bcfd45c062e5defd9707f1f15641a48
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 01:08:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,11 @@ class Route {
       this.endingLocation.vertical - this.beginningLocation.vertical;
       return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
   }
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
 }
