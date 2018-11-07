5349604f8e699dc926327a41404322c6a1ef203b
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-07 01:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Driver {
   yearsExperienceFromBeginningOf(year) {
     let endDate = new Date(year, 1, 1);
     let totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
-    return parseInst(totalYears);
+    return parseInt(totalYears);
   }
 }
 
